8ed0030712c07ac395c5801e4b0a93734a0bf8b5
yohannes-bogale <yoniman2009@gmail.com>
2025-09-15 09:38:48 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ========================================typing animation ================================*/
+var typed = new Typed(".typing",{
+    strings:["", "UX/UI Designer.", "Graphics Designer.", "Web Developer."],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
