17d2fa553b3336eb2f64a83fec9ddebcee9f6265
yohannes-bogale <yoniman2009@gmail.com>
2025-09-15 09:32:19 +0300

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-/* ========================================typing animation ================================*/
-var typed = new Typed(".typing",{
-    strings:["", "UX/UI Designer.", "Graphics Designer.", "Web Developer."],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
